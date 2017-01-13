9f5f51242219ec408ab247455f59841a17f601d8
SjoerdJakobs <18284@ma-web.nl>
2017-01-13 17:14:50 +0100

    quest
    quest

---
+++ Assets/QuestComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestComponent : MonoBehaviour {
+
+    private QuestNode _currentQuestNode;
+
+    [SerializeField]
+    private QuestNode[] _questNodes;
+
+    [SerializeField]
+    private QuestStartPoint _startPoint;
+
+    [SerializeField]
+    private QuestEndPoint _endPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/QuestNode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestNode : MonoBehaviour {
+
+    private bool _active;
+    public bool Active
+    {
+        get { return _active; }
+        set { _active = value; }
+    }
+
+    private bool _completed;
+    public bool Completed
+    {
+        get { return _completed; }
+        set { _completed = value; }
+    }
+
+    private bool _timer;
+    public bool Timer
+    {
+        get { return _timer; }
+        set { _timer = value; }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
